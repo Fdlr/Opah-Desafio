bb0d147c2d039eb1f2b81132ec15aa5071dc16e3
Felipe Rodrigues <fdlr.eng@gmail.com>
2021-03-18 19:16:24 -0300

    setando somente os serviçoes

--- app/src/main/java/com/opah/desafio/felipe/network/api/ApiService.kt
+++ app/src/main/java/com/opah/desafio/felipe/network/api/ApiService.kt
@@ -1,21 +1,12 @@
 package com.opah.desafio.felipe.network.api
 
+import com.opah.desafio.felipe.models.CharacterResponse
 import kotlinx.coroutines.Deferred
+import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
-import retrofit2.http.Query
 
+interface ApiService {
 
-interface MarvelApi {
-
-    @GET("characters")
-    fun getMarvelHeroesAsync(@Query(Definitions.PARAM_LIMIT) limit: Int, @Query(Definitions.PARAM_OFFSET) offset: Int): Deferred<MarvelHeroResponse>
-
-    @GET("characters/{$PARAM_CHARACTER_ID}/comics")
-    fun getMarvelComicsAsync(@Path(PARAM_CHARACTER_ID) heroId: Int): Deferred<MarvelComicsResponse>
-
-    @GET("series")
-    fun getMarvelSeries(@Query(Definitions.PARAM_LIMIT) limit: Int, @Query(Definitions.PARAM_OFFSET) offset: Int): Deferred<MarvelSeriesResponse>
-
+    @GET("v1/public/characters")
+    fun getCharacters(): Deferred<Response<CharacterResponse>>
 }
